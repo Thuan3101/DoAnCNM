a51ecc234f4f3c1440883c1069ef44cc8dc73fbf
Thuan3101 <ngominhnhut6808@gmail.com>
2024-03-02 21:11:42 +0700

    them icon va click avatar

---
+++ my-react-web/src/components/Avatar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/avatar.css';
+
+const Avatar = () => {
+    return (
+        <div className='container'>
+            <div className='row1'>
+            Avatar
+            </div>
+            
+           
+             
+        
+          <div className='row2'></div>
+         
+        </div>
+      );
+    };
+
+export default Avatar;
--- my-react-web/src/components/Home.js
+++ my-react-web/src/components/Home.js
@@ -4,8 +4,16 @@ import CongCu from './CongCu';
 import CaiDat from './CaiDat';
 import Chat from './Chat';
 import Call from './Call';
-import DanhBa from './DanhBa'; // Import DanhBa component
+import DanhBa from './DanhBa'; 
 import '../css/home.css';
+import tool1 from '../image/tool1.png';
+import tool2 from '../image/tool2.png';
+import tool3 from '../image/tool3.png';
+import tool7 from '../image/tool7.png';
+import tool5 from '../image/tool5.png';
+import tool6 from '../image/tool6.png';
+import avatar from '../image/avatar.png';
+import Avatar from './Avatar';
 
 const Home = () => {
   const [currentTab, setCurrentTab] = useState('chat'); // 'chat', 'call', 'cloud', 'congcu', 'caidat', 'danhba'
@@ -17,39 +25,38 @@ const Home = () => {
   return (
     <div className="container1">
       <div className="tabs">
-      <div className='avt'></div>
+        <div onClick={() => handleTabChange('Avatar')}>
+          <img src={avatar} alt="Avatar" className="avatar-home" /></div>
         <div onClick={() => handleTabChange('chat')}>
-          <div className="tab">Chat</div>
+          <img src={tool1} alt="Chat" className="tab-icon" />
         </div>
         <div onClick={() => handleTabChange('call')}>
-          <div className="tab">Call</div>
+          <img src={tool2} alt="Call" className="tab-icon" />
         </div>
         <div onClick={() => handleTabChange('danhba')}>
-          <div className="tab">Danh bạ</div>
+          <img src={tool3} alt="Danh bạ" className="tab-icon" />
         </div>
         <div onClick={() => handleTabChange('cloud')}>
-             <div className="tab" style={{ marginTop: '120px' }}>My Cloud</div>
+          <img src={tool5} alt="My Cloud" className="tab-icon" />
         </div>
         <div onClick={() => handleTabChange('congcu')}>
-          <div className="tab">Công cụ</div>
+          <img src={tool6} alt="Công cụ" className="tab-icon" />
         </div>
         <div onClick={() => handleTabChange('caidat')}>
-          <div className="tab">Cài đặt</div>
+          <img src={tool7} alt="Cài đặt" className="tab-icon" />
         </div>
       </div>
 
       <div className='container2'>
         <div className='tabtop'>
-         
         </div>
         <div className="tab-content">
-        
-         
           {currentTab === 'chat' ? <Chat /> :
+          (currentTab === 'avatar' ? <Avatar /> :
            (currentTab === 'call' ? <Call /> :
            (currentTab === 'danhba' ? <DanhBa /> :
            (currentTab === 'cloud' ? <Cloud /> :
-           (currentTab === 'congcu' ? <CongCu /> : <CaiDat />))))}
+           (currentTab === 'congcu' ? <CongCu /> : <CaiDat />)))))}
         </div>
       </div>
     </div>
